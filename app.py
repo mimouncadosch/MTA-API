081b2be5262f3904258e406ab66c2cf2982da9fd
mimouncadosch <mimouncadosch@gmail.com>
2014-04-15 11:40:19 -0400

    loading data before running server

--- app.py
+++ app.py
@@ -1,40 +1,128 @@
 import os
+from flask import Flask, jsonify, render_template, request, g
+import pickledb
+
+app = Flask(__name__)
+db = pickledb.load('example.db', False)
+# db.set('france', 'paris')
+# db.set('colombia', 'bogota')
+
+ready = 'false'
 
 stop_times = open("google_transit/stop_times.txt", "r+")
 stops_info = open("google_transit/stops.txt", "r+")
-info = open("info.json", "w+")
-
-arrivals = []
 for line in stop_times:
-	t = line.split(",")
-	time = t[1].split(":")
-	id = t[3]
-	arrival = {'time' : time, 'id' : id}
-	arrivals.append(arrival)
-print 'done with arrivals'
+	print line
+	ready = 'true'
 
-stops = []
-for line in stops_info:
-	t = line.split(",")
-	print t
-	id = t[0]
-	name = t[2]
-	lat = t[4]
-	lon = t[5]
+@app.route('/')
+def index():
+	# return 'hello world'
+	return ready
+
+@app.route('/api')
+def api():
+	country = request.args.get('country', 0, type=str)
+	print db.get(country)
+	return db.get(country)
+
+
+# stop_times = open("google_transit/stop_times.txt", "r+")
+# stops_info = open("google_transit/stops.txt", "r+")
+# # info = open("info.json", "w+")
+
+# # arrivals = []
+# for line in stop_times:
+# 	t = line.split(",")
+# 	time = t[1].split(":")
+# 	time = str(time)
+# 	# print 'time', time
+# 	id = t[3]
+# 	# print 'id', id
+# 	# arrival = {'time' : time, 'id' : id}
+# 	# arrivals.append(arrival)
+# 	db.dcreate('stops')
+# 	db.dadd(id, time)
+
+# print 'done with arrivals'
+
+# @app.route('/')
+# def index():
+# 	print db.get('D40S')
+# 	return db.get('D40S')
+
+# # ready = 'false'
+# # arrivals = []
+# # def load_data(callback):
+# # 	global arrivals
+# # 	print 'loading data'
+# # 	data = open("google_transit/stop_times.txt", "r+")
+# # 	for line in data:
+# # 		t = line.split(",")
+# # 		arrivals.append(t)
 	
-	stop = {'id': id, 'name': name, 'lat': lat, 'lon': lon}
-	stops.append(stop)
-print 'done with stops'
+# # 	callback(arrivals)
 
+# # @app.route('/')
+# # def index():
+# # 	def cb(arrivals):
+# # 		print 'done loading file. ready'
+# # 		# print arrivals[0: 100]
+# # 		global ready
+# # 		ready = 'true'
+# # 		print 'ready', ready
+		
+# # 	# while(i )
+# # 	# ready = 'false'
+# # 	# req = request.args.get('ready', 0, type=str)
+# # 	# 	if ready is 'false':
+# # 	# 		return 'false'
+# # 	print 'ready', ready
+# # 	load_data(cb)
 
-data = {}
-for stop in stops:
-	for arrival in arrivals:
-		if stop['id'] == arrival['id']:
-			print 'found match', stop['id'], arrival['id']
-			data[stop['id']] = {'id': stop['id'], 'name': stop['name'], 'lat': stop['lat'], 'lon': stop['lon'], 'time':arrival['time']}
-			print data
+# # 	return render_template('index.html')
 
-data = jsonify(data)
-info.write(data)
+# # @app.route('/check_ready')
+# # def check_ready():
+# # 	global arrivals
+# # 	req = request.args.get('ready', 0, type=str)
+# # 	return jsonify(result=arrivals)
 
+# # # @app.route('/')
+# # # def index():
+# # # 	return render_template('index.html')
+
+# # @app.route('/get_arrivals')
+# # def get_arrivals():
+# # 	print 'callitime = data[1].split(":")
+# # 	# 	print time
+# # 		# hour = time[0]
+# # 		# minute = time[1]ng get_arrivals'
+	
+# # 	currHour = request.args.get('hour', 0, type=str)
+# # 	currMinute = request.args.get('minute', 0, type=str)
+
+# # 	# currArrivals = {} # Dictionary {time, station}
+
+# # 	# for line in arrivals:
+# # 	# 	print line
+# # 	# 	data = line.split(",")
+# # 	# 	# print data
+# # 	# 	
+# # 	# 	if(currHour is hour or currMinute is minute):
+# # 	# 		currArrivals.update({data[3]:(data[1])})
+
+# # 	# return jsonify(result=currHour + currMinute)
+# # 	return jsonify(result=arrivals)
+	
+
+# # # @app.route('/')
+# # # def index():
+# # # 	load_data()
+
+# # # 	# print arrivals
+# # # 	return render_template('index.html')
+
+if __name__ == "__main__":
+	app.run()
+	# main()
